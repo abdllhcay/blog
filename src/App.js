8175e9d30ac988b6ecd8f9fc2242dc47489b7336
abdllhcay <abdllhcay@gmail.com>
2021-01-09 23:03:15 +0300

    basic home page structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Heading,
+  Container,
+  Button,
+  Stack,
+  IconButton,
+  Avatar,
+  Text,
+  Flex,
+  Spacer,
+  Box,
+} from "@chakra-ui/react";
+import { MoonIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxW="2xl" mt={5} centerContent>
+        <Stack direction="row" align="center">
+          {/* <Button colorScheme="teal" variant="ghost">
+            <Avatar
+              name="abdllhcay"
+              src="https://avatars3.githubusercontent.com/u/29143550?s=460&u=19e82a13f6e3a7350d56bc1848b4d7f9337430d8&v=4"
+              size="sm"
+              mr={2}
+            />{" "}
+            abdllhcay
+          </Button> */}
+          {/* <Spacer /> */}
+          <Button colorScheme="teal" variant="ghost">
+            Yazılarım
+          </Button>
+          <Button colorScheme="teal" variant="ghost">
+            Hakkımda
+          </Button>
+          <Button colorScheme="teal" variant="ghost">
+            Yer İmleri
+          </Button>
+          <IconButton
+            aria-label="Night mode"
+            colorScheme="teal"
+            variant="ghost"
+            icon={<MoonIcon />}
+          />
+        </Stack>
+        {/* <Heading as="h3" size="lg">
+        Son Yazılar
+      </Heading> */}
+      </Container>
+      <Container maxW="6xl" mt={20} centerContent>
+        asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd
+      </Container>
     </div>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+export default {
+  fonts: {
+    body: "'Inter', sans-serif",
+    heading: "'Inter', sans-serif",
+    mono: "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace",
+  },
+};
